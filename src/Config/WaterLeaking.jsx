cdb4b29a83a192c4f8b1b709d26a0ae8b1ea4a0e
HazratZahid9454 <zahid454688@gmail.com>
2023-08-09 00:26:35 +0500

    change

--- src/Config/WaterLeaking.jsx
+++ src/Config/WaterLeaking.jsx
@@ -2,21 +2,4 @@
 import waterLeak from '../images/waterLeak.png'
 import powerplug from '../images/powerplug.png'
 
-const waterLeak=[
-    {
-        img:powerplug,
-        text:'No Electricity'
-    },
-    {
-        img:powerplug,
-        text:'No Heating'
-    },
-    {
-        img:waterLeak,
-        text:'Water Leak'
-    }
-
-    
-]
-
 export default waterLeak;
